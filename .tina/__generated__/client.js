29c07c488a6c758dfa057937cfa29ed3d8423e54
chainkloud <chainkloud@gmail.com>
2023-07-26 00:08:21 -0400

    edit

--- .tina/__generated__/client.js
+++ .tina/__generated__/client.js
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'https://content.tinajs.io/1.4/content/36bbcf7c-e3ba-430e-b4d0-7fdca247925b/github/master', token: 'ab9e859263dc92873912f80936f36774c88ce4ba', queries });
+export const client = createClient({ url: 'https://content.tinajs.io/1.4/content/36bbcf7c-e3ba-430e-b4d0-7fdca247925b/github/master', token: 'e1318f0b8a10aff0dab8014a47fb269954e82d19', queries });
 export default client;
   
